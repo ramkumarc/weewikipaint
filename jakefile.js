a704dd063aeb8dd784c4538cb3bb42919b399f77
Ram <ramkchid@yahoo.com>
2013-06-21 12:38:28 -0700

    Build Automation

---
+++ jakefile.js
@@ -0,0 +1,7 @@
+task("default",["lint"]);
+
+desc("Lint everything");
+task("lint",[],function(){
+var lint = require("./build/lint/lint_runner.js");
+    lint.validateFile("jakefile.js",{},{});
+});
