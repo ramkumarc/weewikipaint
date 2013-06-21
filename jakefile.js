d2aa49aefb261fcbdac398975f11e3d855179fa2
Ram <ramkchid@yahoo.com>
2013-06-21 15:02:14 -0700

    Set up conitnuos integration script

--- jakefile.js
+++ jakefile.js
@@ -3,11 +3,12 @@
 (function(){
     "use strict";
 
+    desc("Build and test");
     task("default",["lint"]);
 
     desc("Lint everything");
     task("lint",[],function(){
-    var lint = require("./build/lint/lint_runner.js");
+        var lint = require("./build/lint/lint_runner.js");
 
         var files = new jake.FileList();
         files.include("**/*.js");
@@ -16,6 +17,18 @@
         var options = nodeLintOptions();
         lint.validateFileList(files.toArray(),nodeLintOptions(),{});
     });
+
+   desc("Integrate");
+   task("integrate",["default"],function(){
+       console.log("1. Make sure 'git status' is clean.");
+       console.log("2. Build on the integration box.");
+       console.log("    a. Walk over to integration box.");
+       console.log("    b.'git pull'");
+       console.log("    c. 'jake'");
+       console.log("3. 'git checkout integration'");
+       console.log("4. 'git merge master --no-ff --log'")
+       console.log("5. 'git checkout master'");
+   });
 
     function nodeLintOptions() {
         return {
