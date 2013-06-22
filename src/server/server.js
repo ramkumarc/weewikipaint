0af926a5bd6df7df4b8fa0cd53b88f80af7ca465
Ram <ramkchid@yahoo.com>
2013-06-21 23:41:03 -0700

    First node server js file

---
+++ src/server/server.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ramkumar
+ * Date: 6/21/13
+ * Time: 11:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+console.log("Hello, I'm server !!");
