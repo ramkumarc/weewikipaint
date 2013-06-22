fd929b8161603c0ccc2d8046cfed20e1d87b2750
Ram <ramkchid@yahoo.com>
2013-06-21 23:42:57 -0700

    changed file template

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,7 @@
 /**
- * Created with JetBrains WebStorm.
  * User: ramkumar
  * Date: 6/21/13
  * Time: 11:39 PM
- * To change this template use File | Settings | File Templates.
  */
 
 console.log("Hello, I'm server !!");
