f8c915beb450a2a41dd8beda40272fd247fda4c7
Ram <ramkchid@yahoo.com>
2013-06-21 23:35:00 -0700

    testing continuous integration

--- jakefile.js
+++ jakefile.js
@@ -24,7 +24,7 @@
        console.log("1. Make sure 'git status' is clean.");
        console.log("2. Build on the integration box.");
        console.log("    a. Walk over to integration box.");
-       console.log("    b.'git pull'");
+       console.log("    b. 'git pull'");
        console.log("    c. 'jake'");
        console.log("    d. If jake fails, stop!! Try again after fixing the issue");
        console.log("3. 'git checkout integration'");
